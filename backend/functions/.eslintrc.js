6cfda9270d67531e5f7102766d5ddd8e2f1da9fc
Benjamin Tessem <45benjamin@gmail.com>
2022-10-11 20:32:04 -0600

    Fix up type errors, allow url paramaters

--- backend/functions/.eslintrc.js
+++ backend/functions/.eslintrc.js
@@ -11,22 +11,20 @@ module.exports = {
     "plugin:import/typescript",
     "google",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: ["tsconfig.json", "tsconfig.dev.json"],
-    tsconfigRootDir: "./backend/functions/",
+    tsconfigRootDir: ".",
     sourceType: "module",
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
   },
 };
