99c8f87ecbc026b8f6f9887ef4d726201401938d
JJStephens <siahstephens@gmail.com>
2022-10-18 20:14:50 -0600

    rate widget skeleton

---
+++ frontend/lib/widgets/rate_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:tab_container/tab_container.dart';
+
+class RatingBox extends StatefulWidget {
+  const RatingBox({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => _RatingBoxState();
+}
+
+class _RatingBoxState extends State<RatingBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello");
+  }
+}
