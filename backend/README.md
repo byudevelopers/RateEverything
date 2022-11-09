95e756e1933dec37b6cde1d9146ba7b759fb586a
Conner Vigil <cjvigil@live.com>
2022-11-08 21:03:07 -0700

    Working on users controller

--- backend/README.md
+++ backend/README.md
@@ -1,4 +1,5 @@
-## Setting up Firebase ##
+## Setting up Firebase
+
 1. FIRST CD INTO BACKEND `cd backend`
 2. Install the [Firebase CLI](https://firebase.google.com/docs/cli#install_the_firebase_cli)
 3. Run `firebase login`. If creating a new Firebase account, you should not enter any credit card information.
@@ -6,5 +7,5 @@
 5. Run `firebase emulators:start`
 6. Go to `localhost:4000` in a browser to verify that things appear
 7. Run `cd functions`
-8. Run `npm run serve`
+8. Run `npm run start`
 9. Open `http://localhost:5001/rateeverything/us-central1/helloWorld` in your browser and you should see `Hello from Firebase!`
---
+++ backend/functions/src/routes/sample/usersController.ts
@@ -0,0 +1,32 @@
+import { https, logger } from "firebase-functions";
+import { firestoreDB } from "../../database/firestore";
+import { User } from "./models";
+
+// Get user by id
+export const getUser = https.onRequest(async (request, response) => {
+  const userId = request.query.userId;
+  console.log("userId", userId);
+  const userRef = firestoreDB.collection("users").doc(userId as string);
+  const userDoc = await userRef.get();
+  if (!userDoc.exists) {
+    logger.info("No such document!");
+  } else {
+    logger.info("Document data:", userDoc.data());
+  }
+  response.send(userDoc.data());
+});
+
+// Post user
+export const postUser = https.onRequest(async (request, response) => {
+  let user = request.query.user as User;
+
+  const res = await firestoreDB
+    .collection("users")
+    .doc(user.id as string)
+    .set(user);
+  response.send(res);
+});
+
+// Put user
+
+// Delete user
