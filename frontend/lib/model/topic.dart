90a477f42de8d01014cc4bf4e38d62439648e81a
EvanSmith93 <evan.smith993@gmail.com>
2023-01-24 20:29:13 -0700

    made the leaderboard topics clickable to see their feeds

--- frontend/lib/model/topic.dart
+++ frontend/lib/model/topic.dart
@@ -1,11 +1,13 @@
-// This is mostly just temporary so that we can test the UI
+import 'package:app22_23/model/feed.dart';
 
 class Topic {
   late String question;
   late double averageRating;
+  late Feed feed;
 
-  Topic(this.question, this.averageRating) {
+  Topic(this.question, this.averageRating, this.feed) {
     question = question;
     averageRating = averageRating;
+    feed = feed;
   }
 }
--- frontend/lib/screens/leaderboard/widgets/podium_list.dart
+++ frontend/lib/screens/leaderboard/widgets/podium_list.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:app22_23/model/topic.dart';
+import 'package:app22_23/model/rating.dart';
+import 'package:app22_23/model/feed.dart';
+import 'package:app22_23/screens/feed/main_feed.dart';
 
 class PodiumList extends StatefulWidget {
   PodiumList({Key? key}) : super(key: key);
@@ -10,32 +13,60 @@ class PodiumList extends StatefulWidget {
 
 class _PodiumListState extends State<PodiumList> {
   final List<Topic> _topicList = [
-    Topic("Taylor Swift", 9.7),
-    Topic("Minecraft", 9.3),
-    Topic("Cats", 8.6),
-    Topic("Pineapple on Pizza", 7.9),
+    Topic("Taylor Swift", 9.7, Feed([
+      Rating(9, "thisGuy", "She is the best"),
+      Rating(10, "iAmCool", "She is the very best"),
+    ])),
+    Topic("Cats", 8.6, Feed([
+      Rating(9, "thisGuy", "Cats are my favorite"),
+      Rating(8, "iAmCool", "Cats are the very best"),
+    ])),
+    Topic("Pineapple on Pizza", 7.9, Feed([
+      Rating(8, "thisGuy", "Pineapple on pizza is the best"),
+      Rating(8, "iAmCool", "Pineapple on pizza is cool"),
+    ])),
   ];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: _topicList.length,
       itemBuilder: (context, position) {
-        return Card(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  _topicList[position].question,
-                ),
-                Text(
-                  _topicList[position].averageRating.toString(),
-                ),
-              ]
-            )
-          ),
-        );
+        return GestureDetector(
+            child: Card(
+              child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          _topicList[position].question,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              _topicList[position].averageRating.toString(),
+                            ),
+                            const Icon(
+                              Icons.chevron_right,
+                              size: 24,
+                            ),
+                          ],
+                        ),
+                      ])),
+            ),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => 
+                      Scaffold(
+                        appBar: AppBar(
+                          title: const Text("Feed Screen"),
+                        ),
+                        body: MainFeed(feed: _topicList[position].feed)
+                      )
+                  ));
+            });
       },
     );
   }
